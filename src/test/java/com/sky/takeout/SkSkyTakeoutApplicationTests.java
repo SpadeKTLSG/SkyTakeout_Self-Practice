9245a0c935f3b2774ea1fe862a5a4fb0476c73c9
SpadeKtlsg <spadekxcwxtlsg@gmail.com>
2023-11-30 21:32:03 +0800

    吼吼吼

---
+++ src/test/java/com/sky/takeout/SkSkyTakeoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.takeout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkSkyTakeoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
